1976bae465e9446239d0cb953a201a1d7bb4cb99
Dika20704 <ramaxdika.gmail.com>
2026-06-13 14:33:25 +0700

    daya1

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('.toggle').addEventListener('click',function(){
+    const sidebar = document.querySelector('.sidebar')
+sidebar.classlist.add('.show')
+})
+
+const scrollCard = document.querySelector('.cards')
+
+scrollCard.addEventListener('mousedown',function(e){
+
+})
